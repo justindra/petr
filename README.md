e08875a1f925e0bdfc82264107482f80e1c3a095
Justin Rahardjo <justindra91@gmail.com>
2023-05-17 15:42:20 -0700

    feat: added runner

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # PETR (Prompt Engineering Test Reporter)
+
+PETR allows you to run multiple models with the same prompt and then compare the results to each other. Currently designed to be used with [LangChain.js](https://github.com/hwchase17/langchainjs).
+
+## Installation
+
+```bash
+pnpm add petr
+```
+
+## Usage
+
+```typescript
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from 'langchain/prompts';
+import { runner } from 'petr';
+
+const CHAT_PROMPT = new ChatPromptTemplate({
+  promptMessages: [
+    new SystemMessagePromptTemplate(
+      new PromptTemplate({
+        template: `You are JokeBot. You are a bot that tells jokes!`,
+      })
+    ),
+    new HumanMessagePromptTemplate(
+      new PromptTemplate({
+        template: `{input}`,
+        inputVariables: ['input'],
+      })
+    ),
+  ],
+  inputVariables: ['input'],
+});
+
+const STANDARD_PROMPT = new PromptTemplate({
+  template: `${systemPrompt}\n\nuser: {input}\nJokeBot:`,
+  inputVariables: ['input'],
+});
+
+export const PROMPT_SELECTOR = /*#__PURE__*/ new ConditionalPromptSelector(
+  CHAT_PROMPT,
+  [[isLLM, STANDARD_PROMPT]]
+);
+
+await runner({
+  prompt: PROMPT_SELECTOR,
+  data: [{ input: 'tell me a joke!' }, { input: 'What else can you tell me?' }],
+  csvParams: { path: 'jokes.csv' },
+  loadChainFn: (llm, promptSelector) =>
+    LLMChain({
+      prompt: promptSelector.getPrompt(llm),
+      llm,
+      outputKey: 'output',
+    }),
+  models: [
+    {
+      name: 'gpt-4',
+      llm: new ChatOpenAI({ temperature: 0.9, modelName: 'gpt-4' }),
+    },
+    {
+      name: 'gpt-35',
+      llm: new ChatOpenAI({ temperature: 0.9, modelName: 'gpt-3.5-turbo' }),
+    },
+  ],
+});
+```
+
+This will then output a `jokes.csv` file that should look like the following:
+
+```csv
+input,gpt-4,gpt-35
+tell me a joke!,What do you call a cow with no legs?,What do you call a cow with no legs?
+What else can you tell me?,What do you call a cow with no legs?,What do you call a cow with no legs?
+```
---
+++ packages/petr/src/runner.ts
@@ -0,0 +1,152 @@
+import { createObjectCsvWriter } from 'csv-writer';
+import { BaseLanguageModel } from 'langchain/base_language';
+import { LLMChain } from 'langchain/chains';
+import { ConditionalPromptSelector } from 'langchain/prompts';
+
+/**
+ * Duplicated from csv-writer as the types are not exported
+ */
+type ObjectHeaderItem = { id: string; title: string };
+type ObjectStringifierHeader = ObjectHeaderItem[] | string[];
+
+interface ObjectCsvWriterParams {
+  path: string;
+  header: ObjectStringifierHeader;
+  fieldDelimiter?: string;
+  recordDelimiter?: string;
+  headerIdDelimiter?: string;
+  alwaysQuote?: boolean;
+  encoding?: string;
+  append?: boolean;
+}
+
+type ModelOption = { name: string; llm: BaseLanguageModel };
+
+type RunnerParams<
+  TInput extends Record<string, any>,
+  TOutput extends Record<string, string>
+> = {
+  /** The prompt to pass into the chain */
+  prompt: ConditionalPromptSelector;
+  /** The input data to run over */
+  data: TInput[];
+  /**
+   * A function that loads a chain, given a language model and a conditional
+   * prompt selector.
+   */
+  loadChainFn: (
+    llm: BaseLanguageModel,
+    prompt: ConditionalPromptSelector
+  ) => Promise<LLMChain>;
+  /**
+   * Converter to turn the provided input into something that can be used to
+   * call the chain.
+   */
+  inputConverter?: (data: TInput) => any;
+  /**
+   * Converter to turn the output of the chain into what is written into the
+   * CSV file. By default, it just combines the input and output data.
+   * @param inputData The input data
+   * @param outputData The output data from the chains
+   * @returns
+   */
+  outputConverter?: (
+    inputData: TInput,
+    outputData: Record<string, any>
+  ) => TOutput;
+  /**
+   * Any additional parameters to pass into the CSV writer
+   */
+  csvParams: Partial<ObjectCsvWriterParams>;
+  models: ModelOption[];
+};
+
+/**
+ * Creates the CSV Writer to use for the runner based on the provided input
+ * parameters.
+ * @returns
+ */
+function getCSVWriter<
+  TInput extends Record<string, any>,
+  TOutput extends Record<string, any>
+>({
+  csvParams,
+  data,
+  models,
+  outputConverter,
+}: Required<
+  Pick<
+    RunnerParams<TInput, TOutput>,
+    'csvParams' | 'data' | 'outputConverter' | 'models'
+  >
+>) {
+  const fakeOutput: Record<string, any> = {};
+  models.forEach(({ name }) => (fakeOutput[name] = ''));
+
+  return createObjectCsvWriter({
+    path: 'output.csv', // Default output file name, if not specified
+    ...csvParams,
+    header: csvParams.header?.length
+      ? csvParams.header
+      : Object.keys(outputConverter(data[0], fakeOutput)).map((val) => ({
+          id: val,
+          title: val.toUpperCase(),
+        })),
+  });
+}
+
+/**
+ * Runs a full set of classifications over a set of data given a prompt to use.
+ * This will run it across 3 different models, and then combine the results into
+ * a single CSV file.
+ */
+export async function runner<
+  TInput extends Record<string, any>,
+  TOutput extends Record<string, any>
+>({
+  prompt,
+  data,
+  loadChainFn,
+  inputConverter = (data: TInput) => data,
+  outputConverter = (inputData: TInput, outputData: Record<string, any>) =>
+    ({
+      ...inputData,
+      ...outputData,
+    } as TOutput),
+  csvParams,
+  models,
+}: RunnerParams<TInput, TOutput>) {
+  const chains = await Promise.all(
+    models.map(async ({ name, llm }) => ({
+      name,
+      chain: await loadChainFn(llm, prompt),
+    }))
+  );
+
+  const csvWriter = getCSVWriter({ csvParams, data, models, outputConverter });
+
+  const results: TOutput[] = [];
+  let i = 0;
+
+  for (const datum of data) {
+    const input = inputConverter(datum);
+    const res: Record<string, any> = {};
+
+    for (const chain of chains) {
+      const classification = (await chain.chain.call(input))['output'];
+      res[chain.name] = classification;
+    }
+
+    const output = outputConverter(datum, res);
+
+    results.push(output as TOutput);
+    i++;
+
+    console.log(`Finished ${i} of ${data.length}`);
+    await csvWriter.writeRecords([output]);
+    break;
+  }
+
+  console.log('Finished!');
+  return { results };
+}
